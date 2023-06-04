16ffb77619e57e0b10ec14c17e8d9b8054efdbbd
Ahmet <ahmetcanaydin1990@gmail.com>
2023-06-04 21:04:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector("button");
+let inpt = document.querySelector("#myInput");
+const screen = document.querySelector(".alert");
+let number = Math.round(Math.random() * 100);
+let biggerGuess = 100,
+  smallGuess = 0,
+  counter = 0;
+btn.addEventListener("click", guessGame);
+btn.addEventListener("click", guessGame);
+function guessGame() {
+  let guess = inpt.value;
+  counter++;
+  if (guess < 0 || guess > 100) {
+    screen.innerText = `The number you have enetered must be a number between 0-100`;
+  } else {
+    if (number == guess) {
+      screen.innerText = `You got it right on ${counter}. guess.👏`;
+      number = Math.round(Math.random() * 100);
+      counter = biggerGuess = smallGuess = guess = 0;
+    } else if (guess < number) {
+      smallGuess = guess;
+      screen.innerText = `Enter a number between ${guess} - ${biggerGuess}`;
+    } else {
+      biggerGuess = guess;
+      screen.innerText = `Enter a number between ${smallGuess} - ${guess}`;
+    }
+  }
+}
